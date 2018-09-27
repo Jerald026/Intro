4c88f6aa831da8dcbaf12dd711b2a556d0b62ece
Student <Student@L411-S15>
2018-09-27 15:32:45 +0800

    Initial Codes

--- src/jerald/Jerald.java
+++ src/jerald/Jerald.java
@@ -4,6 +4,7 @@ public class Jerald {
 
     public static void main(String[] args) {
         System.out.println("Sample Project");
+        System.out.println("Sample Project 2");
                 
     }
     
